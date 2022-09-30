6de8f786393f6ff996d47a8e81816dba27a2a0cd
Getaneh48 <getaneh11@gmail.com>
2022-09-30 12:05:15 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - prints the number of arguments passed into it.
+*
+* @argc: number of arguments.
+* @argv: collection of arguments.
+*
+* Return: 0 success 1 for failor.
+*
+*/
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
