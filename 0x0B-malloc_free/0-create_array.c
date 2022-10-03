9dc9e7f9cda95f8c5a4b97094b87928df995d949
Getaneh48 <getaneh11@gmail.com>
2022-10-03 13:42:43 +0300

    Task 0 betty fixed.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -24,11 +24,9 @@ char *create_array(unsigned int size, char c)
 
 	if (ptr == NULL)
 		return (NULL);
-	else
-	{
-		for (i = 0; i < size; i++)
-			*(ptr + i) = c;
-	}
+
+	for (i = 0; i < size; i++)
+		*(ptr + i) = c;
 
 	return (ptr);
 }
