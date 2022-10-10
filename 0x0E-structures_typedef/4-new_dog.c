b6921d77c9629bdeabaeb43a0e8b094eab51f348
Getaneh48 <getaneh11@gmail.com>
2022-10-10 14:42:52 +0300

    task 4 bug fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
 * new_dog - create a new dog structure.
@@ -18,9 +19,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	newdog = malloc(sizeof(dog_t));
 
+	if (newdog == NULL)
+		return (NULL);
+
 	newdog->name = name;
-	newdog->age = age;
 	newdog->owner = owner;
+	newdog->age = age;
 
 	return (newdog);
 }
