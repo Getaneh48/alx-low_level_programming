1dec40ce4b059261a6df3bbf82326a005869d327
Getaneh48 <getaneh11@gmail.com>
2022-10-10 12:24:01 +0300

    Task 0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* dog - a structure representing dog.
+*
+* @name: name of the dog.
+* @age: age of the dog.
+* @owner: owner of the dog.
+*
+*/
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+#endif
