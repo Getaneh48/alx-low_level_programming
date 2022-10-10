dfb24e26dd73dd1577fe34df6942e816f7c557f3
Getaneh48 <getaneh11@gmail.com>
2022-10-10 14:03:10 +0300

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog -  function that frees memory used by the stucture.
+*
+* @d: a structure of type dog
+*
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d);
+	}
+}
