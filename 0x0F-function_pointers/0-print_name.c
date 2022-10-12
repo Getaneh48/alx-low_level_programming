4293dacc0b63a939c260a935882361023a31190f
Getaneh48 <getaneh11@gmail.com>
2022-10-12 14:31:25 +0300

    Task 0 big fixed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
 * print_name - primts a name
@@ -10,5 +11,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+    if (name != NULL)
+        f(name);
 }
