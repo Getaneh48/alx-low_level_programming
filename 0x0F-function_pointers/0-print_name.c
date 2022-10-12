f961a22f5e906287cc645857ca42076c44d38fd1
Getaneh48 <getaneh11@gmail.com>
2022-10-12 14:23:15 +0300

    Task betty fixed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,13 @@
 #include "function_pointers.h"
 
+/**
+* print_name - primts a name
+*
+* @name: name to print.
+* @f: function pointer
+*
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
     f(name);
