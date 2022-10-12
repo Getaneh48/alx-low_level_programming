7c36d780399bee941f1e6f5641cb09166d0cf1f4
Getaneh48 <getaneh11@gmail.com>
2022-10-12 18:42:22 +0000

    Task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - entry point of the program.
+*
+* @argc: number of arguments.
+* @argv: lists of arguments.
+*
+* Return: 0 on success 1 on failure.
+*
+*/
+
+int main(int argc, char *argv[])
+{
+	int x, y, result;
+	char *opr;
+	int (*f)(int, int);
+
+	if (argc == 4)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[3]);
+		opr = argv[2];
+
+		if ((strcmp(opr, "/") == 0 || strcmp(opr, "%") == 0) && y == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+
+		f = get_op_func(opr);
+		if (f == NULL)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+
+		result = f(x, y);
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include "3-calc.h"
+
+/**
+* op_add - adds two numbers.
+*
+* @a: integer argument.
+* @b: integer argument.
+*
+* Return: the sum of a and b.
+*
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - subtracts two number.
+*
+* @a: integer argument.
+* @b: integer argument.
+*
+* Return: the subtract of a and b.
+*
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - multiplies two numbers.
+*
+* @a: integer argument.
+* @b: integer argument.
+*
+* Return: the multiplication of a and b.
+*
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - devides two numbers.
+*
+* @a: integer argument.
+* @b: integer argument.
+*
+* Return: the division of two numbers.
+*
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - calculate modules of two numbers.
+*
+* @a: integer argument.
+* @b: integer argument.
+*
+* Return: modules of a and b.
+*
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
