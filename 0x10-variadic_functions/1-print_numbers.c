33db71db588be1cb3c88182ee66a860d46ac0bca
Getaneh48 <getaneh11@gmail.com>
2022-10-13 16:46:05 +0000

    Task 1 betty fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 
 /**
 * print_numbers - prints numbers, followed by a new line.
-* 
+*
 * @separator: number separator charactor.
 * @n: number of arguments.
 *
@@ -11,24 +11,23 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i;
-    va_list args;
+	unsigned int i;
+	va_list args;
 
-    va_start(args, n);
+	va_start(args, n);
 
-    if (separator != NULL)
-    {
-        for (i = 0; i < n; i++)
-        {
-        
-            if (i < n - 1)
-                printf("%d%s", va_arg(args, int), separator);
-            else
-                printf("%d", va_arg(args, int));
-        }
-    }
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (i < n - 1)
+				printf("%d%s", va_arg(args, int), separator);
+			else
+				printf("%d", va_arg(args, int));
+		}
+	}
 
-    printf("\n");
+	printf("\n");
 
-    va_end(args);
+	va_end(args);
 }
