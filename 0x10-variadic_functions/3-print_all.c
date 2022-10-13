9125ba97587a2d60fcd96cabe2fdfbb6f3382392
Getaneh48 <getaneh11@gmail.com>
2022-10-13 19:00:55 +0000

    Task 3 bug fixed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,50 +2,53 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void print_all(const char * format, ...)
+/**
+* print_all - prints anything.
+*
+* @format: format specifier.
+*
+*/
+
+void print_all(const char * const format, ...)
 {
-    va_list args;
-    char ch;
-    int d;
-    int c;
-    double f;
-    char *s;
-    int match = 1;
+	va_list args;
+	char ch, *s;
+	int d, c, i = 0, match = 1;
+	double f;
 
-    va_start(args, format);
-    while((ch = *format))
-    {
-        switch(ch)
-        {
-            case 'c':
-                c = va_arg(args, int);
-                printf("%c", c);
-                break;
-            case 'i':
-                d = va_arg(args, int);
-                printf("%d", d);
-                break;
-            case 'f':
-                f = va_arg(args, double);
-                printf("%f", f);
-                break;
-            case 's':
-                s = va_arg(args, char *);
-                if (s != NULL)
-                    printf("%s", s);
-                else
-                    printf("%s", "(nil)");
-                break;
-            default:
-                match = 0;
-                break;
-        }
-        format++;
-        if (*format != '\0' && match)
-            printf(", ");
-
-        match = 1;
-    }
-
-    printf("\n");
+	va_start(args, format);
+	while ((ch = format[i]))
+	{
+		switch (ch)
+		{
+			case 'c':
+				c = va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				d = va_arg(args, int);
+				printf("%d", d);
+				break;
+			case 'f':
+				f = va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+				{
+					printf("%s", "(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
+			default:
+				match = 0;
+		} i++;
+		if (format[i] != '\0' && match)
+			printf(", ");
+		match = 1;
+	}
+	printf("\n");
+	va_end(args);
 }
