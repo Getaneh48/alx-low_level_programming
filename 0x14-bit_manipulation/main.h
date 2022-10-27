e83f03fadbf304b46414118a10f458970071445c
Getaneh48 <getaneh11@gmail.com>
2022-10-27 14:19:02 +0300

    Task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stddef.h>
-int _putchar(int);
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 int _pow(int base, int exp);
 unsigned int _strlen(const char *n);
+void print_binary(unsigned long int n);
 #endif
