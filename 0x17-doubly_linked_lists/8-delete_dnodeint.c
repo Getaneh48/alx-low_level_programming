c9d2ca91fdc0b626316232835fc7f3c12a740eae
Getaneh48 <getaneh11@gmail.com>
2022-12-08 20:18:08 +0300

    Task 8 fixed

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -26,6 +26,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 					(*head)->prev = NULL;
 				}
 			}
+			else if (h->next == NULL)
+			{
+				h->prev->next = NULL;
+			}
 			else
 			{
 				h->prev->next = h->next;
