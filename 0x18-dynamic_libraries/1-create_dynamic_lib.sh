433f79e2a4d080554b92830b54873d0ee7b31572
Getaneh48 <getaneh11@gmail.com>
2024-01-15 17:51:01 +0300

    comment added at the top

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# a script to creates a dynamic link library called liball.so
+# from all the .c files that are in the current directory
+
 gcc -c fPIC *.c
 gcc -shared -o liball.so *.o
